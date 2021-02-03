e9d86a342b73f1b7e420b92891ab7bc7e6d2c13f
hcheng2 <76693746+hcheng2@users.noreply.github.com>
2021-02-03 14:50:55 -0500

    Update AgoraRTC.js

--- AgoraRTC.js
+++ AgoraRTC.js
@@ -1,24 +1,13 @@
 let local = []; // stores the UIDs of local streams 
+let username = "" , channelName = "";
+let globalStream;
+let numScreens = 0; 
 
 let handleFail = function(err){
     console.log("Error: ", err);
 };
 
-let participants = "";
-let numRemote = 0;
-
-let numParticipants = 0;
-
-let remoteContainer = document.getElementById("remote-stream");
-
-// adds participant to list of participants
-function addParticipant(name){
-    participants += name;
-    participants += "<br>";
-
-   document.getElementById("participants").innerHTML = participants;    
-}
-
+// self video stream 
 let appID = "2400b34296a64a5295504ebc9e2a741f";
 let client = AgoraRTC.createClient({
         mode: "live",
@@ -26,20 +15,12 @@ let client = AgoraRTC.createClient({
 });  
 client.init(appID);
 
-let username = "" , channelName = "";
-
-let globalStream;
-
-
 function startSelfStream(){
-    numParticipants++;
+    numScreens++;
 
     username = sessionStorage.getItem("name");  
     channelName = sessionStorage.getItem("channel");
 
-//    addParticipant(username);
-
-    // 2--join
     client.join(null, channelName, null, (uid)=>{
         let localStream = AgoraRTC.createStream({
             audio: true,
@@ -53,9 +34,9 @@ function startSelfStream(){
         }, handleFail);
         globalStream = localStream;
     }, handleFail);
-    
 }
 
+// self screen share
 let screenClient = AgoraRTC.createClient({
     mode: "live",
     codec: "h264"  
@@ -74,55 +55,28 @@ function shareScreen(){
             });
             screenId = String(uid);
             local.push(String(uid));
-        //    alert(local);
-            
+
             screenStream.init(()=>{
                 screenStream.play("self-screen");
                 screenClient.publish(screenStream, handleFail);              
         }, handleFail);
     }, handleFail);
 }
-/*
-screenClient.on("stream-added", function(evt){
-    
-    alert("screen added 1");
-
-});
-
-screenClient.on("stream-subscribed", function(evt){
-    let stream = evt.stream;
-    let streamId = String(stream.getId());
-    if(!localStreams.includes(streamId)) {
-        screenClient.subscribe(evt.stream, handleFail);
-
-        addScreenStream(streamId);
-        stream.play(streamId);    
-    }
-
-
-
-});*/
-
 
 client.on("stream-added", function(evt){
     // close window if there are too many participants 
-    if(numParticipants > 3){
+    if(numScreens > 3){
         alert("This channel is full. Please select a different channel");
         window.close();            
     }
-    numParticipants++;
+    numScreens++;
 
     client.subscribe(evt.stream, handleFail);
 });
 
 client.on("stream-subscribed", function(evt){
-
-
         let stream = evt.stream;
         let id = String(stream.getId());
-    //  alert(streamId);
-  //  alert(local + " " + id + " "+ !(local.includes(id)));
-
     if(!local.includes(id)) {
 
         addVideoStream(id);
@@ -130,20 +84,8 @@ client.on("stream-subscribed", function(evt){
     }
 });
 
-
-
-
-function closeWindow(){
-    window.close();
-}
-
-function invite(){
-    alert("Link: https://hcheng2.github.io/Agora-Final-Project.github.io/ \nChannel Name: " + channelName);
-}
-
-
 function removeVideoStream(elementId) {
-    numParticipants--;
+    numScreens--;
     let remoteDiv = document.getElementById(elementId);
     if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv);
 };
@@ -187,6 +129,14 @@ function mute(){
         globalStream.unmuteAudio();
         isaudioMuted = false;
     }
+}
+
+function closeWindow(){
+    window.close();
+}
+
+function invite(){
+    alert("Link: https://hcheng2.github.io/Agora-Final-Project.github.io/ \nChannel Name: " + channelName);
 }
 
 
